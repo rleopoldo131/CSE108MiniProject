6cacadfaec1908f464bfad668dcc54bfa73e7efb
edquan0904 <edquan0904@gmail.com>
2025-04-18 13:29:14 -0700

    Update course.py

--- CSE108MiniProject1/backend/app/models/course.py
+++ CSE108MiniProject1/backend/app/models/course.py
@@ -3,10 +3,24 @@
 from app.database import db
 from app.models.users import User
 
+enrollment = db.Table(
+    'enrollment',
+    db.Column('student_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
+
 class Course(db.Model):
+    __tablename__ = "course"
     id = db.Column(db.Integer, primary_key=True)#setting id column in db
     title = db.Column(db.String(100))#setting the title of course
     capacity = db.Column(db.Integer)#showing class size i db
     time = db.Column(db.String(50))#showing shcdule of class
     teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'))#showing teacher id for each course
     teacher = db.relationship('User', foreign_keys=[teacher_id])#showing relationship between techer and teacher_id
+    students = db.relationship(
+        "User",
+        secondary=enrollment,
+        backref="enrolled_courses"
+    )
+
+    grades = db.relationship("Grade", backref="course")
